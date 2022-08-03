1388628849e47680779bf5459e4b67fcda8f8125
kevin <kevinchen@yeahka.com>
2022-08-03 14:26:26 +0800

    feat: 环境控制

--- src/utils/globalConfig.js
+++ src/utils/globalConfig.js
@@ -2,9 +2,11 @@ let env = process.env.CUSTOM_ENV || 'test'
 let isTest = env === 'test'
 let subfix = isTest? '_test': ''
 
-export const tables = {
+const table = {
   recordsTable: `records${subfix}`,
   savingTable: `saving${subfix}`,
   savingTableId: `${isTest? 'recFolhzu0j0V2ADk': 'reca22Hd66BFUBolR'}`,
   enumTable: `enumeration`
 }
+
+export default table
